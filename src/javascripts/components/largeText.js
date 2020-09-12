7b2feedcd0a7990f92ded5c1152840a6c4d9610d
Abbey Brown <abbeybrownsemail@gmail.com>
2020-09-12 13:51:41 -0500

    working on large text

--- src/javascripts/components/largeText.js
+++ src/javascripts/components/largeText.js
@@ -1,5 +1,7 @@
 const largeText = () => {
   $('#large-text').change(() => {
+    $('body').attr('style', '');
+
     document.body.classList.toggle('large--text');
     document.body.classList.toggle('dark--mode');
   });
